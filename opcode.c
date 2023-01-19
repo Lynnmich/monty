2134e9338e6fd813c672ec9c8af98b9dd1b057f7
Lynnmich <lynnenmichuki@gmail.com>
2023-01-19 01:22:08 -0800

    Op code

--- opcode.c
+++ opcode.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "header.h"
+#include "monty.h"
 
 /**
  * opcode - function in charge of running builtins
  * @stack: stack given by main
  * @str: string to compare
- * @line_cnt: ammount of lines
+ * @line_count: amount of lines
  *
  * Return: nothing
  */
-void opcode(stack_t **stack, char *str, unsigned int line_cnt)
+void opcode(stack_t **stack, char *str, unsigned int line_count)
 {
 	int i = 0;
 
@@ -32,11 +32,11 @@ void opcode(stack_t **stack, char *str, unsigned int line_cnt)
 	{
 		if (strcmp(op[i].opcode, str) == 0)
 		{
-			op[i].f(stack, line_cnt);
+			op[i].f(stack, line_count);
 			return; /* if we found a match, run the function */
 		}
 		i++;
 	}
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_cnt, str);
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_count, str);
 	status = EXIT_FAILURE;
 }
